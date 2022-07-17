bf273cf90adce8bcab5d69f647c01ffcdb8ca200
TheDoctor <merrilldsilva@gmail.com>
2022-07-17 22:31:11 +0530

    19

--- dojo_r/src/App.js
+++ dojo_r/src/App.js
@@ -1,5 +1,4 @@
 
-import './App.css';
 import Navbar from './navbar';
 import Home from './home';
 
--- dojo_r/src/home.js
+++ dojo_r/src/home.js
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import BlogList from "./blogList";
 const Home = () => {
-    return ( 
+    const [blogs, setBlogs] = useState(null);
+    const[isloading, setisloading]= useState(true);
+    const[err,seterr]=useState(null);
+   useEffect(()=>{
+
+        fetch('http://localhost:8000/blogs')
+        
+        .then(res =>{
+            return res.json();
+        })
+        .then(data=>{
+            setBlogs(data);
+            setisloading(false);
+        })
+        .catch(err=>{
+            seterr('could not connect');
+            setisloading(false);
+
+        });
+  
+
+    }, []);
+    
+    return (
             <div className="home">
-                <h2>Homepage</h2>
+                {err&&<div>{err}</div>}
+                {isloading&&<div>loading...</div>}
+                {blogs&&<BlogList blogs={blogs} title="All Blogs"/>}
             </div>
-     );
+                 );
 }
  
 export default Home ;
